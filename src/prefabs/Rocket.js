305e3d56d1a8461d851dd9276a0b19842966cb35
Horacio <hocastil@ucsc.edu>
2022-04-24 12:58:19 -0700

    added two sounds

--- src/prefabs/Rocket.js
+++ src/prefabs/Rocket.js
@@ -5,6 +5,7 @@ class Rocket extends Phaser.GameObjects.Sprite {
         scene.add.existing(this); // add object to existing scene
         this.isFiring = false;
         this.moveSpeed = 2;
+        this.sfxRocket = scene.sound.add('sfx_rocket'); // add rocket sfx
     }
 
     update() {
@@ -17,8 +18,9 @@ class Rocket extends Phaser.GameObjects.Sprite {
             }
         }
         // fire button
-        if(Phaser.Input.Keyboard.JustDown(keyF)) {
+        if (Phaser.Input.Keyboard.JustDown(keyF) && !this.isFiring) {
             this.isFiring = true;
+            this.sfxRocket.play();  // play sfx
         }
         // if fired, move
         if(this.isFiring && this.y >= borderUISize * 3 + borderPadding) {
